d9094297e7201c1190dc1d5af7fb3ba147f9bccc
Hypertera <hypertera2@gmail.com>
2026-09-03 09:00:29 -0600

    Add files via upload

---
+++ Python/MT6701.py
@@ -0,0 +1,35 @@
+# %% Inicio
+import time
+import math
+import keyboard
+from Comandos import Motores, Sensores
+PI = math.pi
+
+mot = Motores()
+mot.conectarArd() 
+sen = Sensores()
+sen.conectarArd()
+
+# %% Calibrar Sensor
+
+ang = sen.obtenerPosicion() 
+desfase = ang[0]
+invertido = False
+sen.enviarCal(0, desfase, invertido)
+
+# %% Mover Motor y leer Sensor
+
+mot.enviarPosArt([0,  -4*PI - (3/4)*PI])   
+
+while True:
+    if keyboard.is_pressed('space'): # Presiona espacio para terminar el programa
+        break
+    ang = sen.obtenerPosicion() 
+    print(f"{ang[0]}\n")
+    time.sleep(0.016666)
+
+
+# %% Cerrar comunicación serial
+
+sen.cerrarSerial()
+mot.cerrarSerial()
